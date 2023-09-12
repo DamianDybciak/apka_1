ea1ee2ddbe86e071fd694db07f1a4ae8f8ff73a8
DamianDybciak <damian.dybciak00@gmail.com>
2023-09-12 16:11:29 +0200

    changed

--- aplikacja.php
+++ aplikacja.php
@@ -6,7 +6,9 @@
     <title>Document</title>
 </head>
 <body>
+    <? echo"Zaloguj się ";?> 
 <form method="GET" action="aplikacja.php">
+
 
     Login <input type="text" name="login" size="20" maxlength="25"> <br>
     Hasło <input type="password" name="haslo" size="20" maxlength="25"> <br>
