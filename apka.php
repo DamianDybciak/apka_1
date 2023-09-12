40ce0f6bf019f903b549c1b1908e36cc9bd299e2
DamianDybciak <damian.dybciak00@gmail.com>
2023-09-12 12:33:19 +0200

    Created login menu

---
+++ apka.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM</title>
+</head>
+<body>
+   
+    <form method="POST" action="apka.php">
+
+        Login <input type="text" name="login" size="20" maxlength="25"> <br>
+        Hasło <input type="password" name="haslo" size="20" maxlength="25"> <br>
+        <input type="submit" value="Zaloguj">
+    </form>
+    
+</body>
+</html>
