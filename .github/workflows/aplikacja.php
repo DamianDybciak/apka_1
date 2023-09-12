b2dc020d57f989de623ed267d21875ba4f1c4fd0
DamianDybciak <damian.dybciak00@gmail.com>
2023-09-12 14:53:57 +0200

    change

---
+++ .github/workflows/aplikacja.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="GET" action="aplikacja.php">
+
+    Login <input type="text" name="login" size="20" maxlength="25"> <br>
+    Hasło <input type="password" name="haslo" size="20" maxlength="25"> <br>
+    <input type="submit" value="Zaloguj">
+</form>
+</body>
+</html>
